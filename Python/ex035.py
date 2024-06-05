238361c2681e77764ac883ea53219e8db777fac8
Lucas Gabriel <lucas20240029181@alu.uern.br>
2024-06-05 18:33:46 -0300

    Update ex35
    Corrigindo apenas a ortografia do código.

--- Python/ex035.py
+++ Python/ex035.py
@@ -1,3 +1,4 @@
+
 print("--=" * 10)
 print("Analisador de Triângulos")
 print("--=" * 10)
@@ -9,5 +10,4 @@ n3 = float(input("Terceiro segmento: "))
 if n1 + n2 > n3 and n2 + n3 > n1 and n1 + n3 > n2:
     print("Os segmentos acima PODEM FORMAR UM TRIÂNGULO!")
 else:
-     print("Os segmentos acima NÃO PODEM FORMAR UM TRIÂNGULO!")
-     
+    print("Os segmentos acima NÃO PODEM FORMAR UM TRIÂNGULO!")
